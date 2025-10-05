3a696d598b35f4f34bd6f664c5c7452b89ae0f05
TioSniperxD <creaducuenta6@hotmail.com>
2025-10-05 18:47:56 -0500

    Commit terminando clase Principal

--- Principal.java
+++ Principal.java
@@ -1,5 +1,15 @@
 public class Principal {
     public static void main(String[] args) {
-        
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.listarLibros();
+        biblioteca.listarUsuarios();
+        biblioteca.buscarLibroPorISBN("100");
+        biblioteca.buscarUsuarioPorID("U001");
+        Usuario usuario = biblioteca.buscarUsuarioPorID("U001");
+        Libro libro = biblioteca.buscarLibroPorISBN("100");
+        usuario.tomarPrestado(libro);
+        biblioteca.listarLibros();
+        usuario.devolverLibro(libro);
+        biblioteca.listarLibros();
     }
 }
